5ec420fe011ad314acd287d05fdb4b728816d145
Gabriel Bitti <gabrielbitti0@gmail.com>
2019-04-16 08:39:01 -0300

    Cria lógica e métodos para realizar os cálculos

--- GasolineOrAlcohol/GasolineOrAlcohol/ViewController.swift
+++ GasolineOrAlcohol/GasolineOrAlcohol/ViewController.swift
@@ -15,6 +15,47 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var gasolineTxt: UITextField!
+    @IBOutlet weak var alcoholTxt: UITextField!
+    
+    @IBAction func caltulate(_ sender: Any) {
+        if let alcoholValue = alcoholTxt.text {
+            if let gasolineValue = gasolineTxt.text {
+                let result = self.validate(alcoholValue: alcoholValue, gasolineValue: gasolineValue)
+                
+                if result {
+                    self.calculateBestPrice(alcoholValue: alcoholValue, gasolineValue: gasolineValue)
+                } else {
+                    resultLabel.text = "Digite os preços para calcular!"
+                }
+            }
+        }
+    }
+    
+    func calculateBestPrice(alcoholValue: String, gasolineValue: String) {
+        if let alcoholValue = Double(alcoholValue) {
+            if let gasolineValue = Double(gasolineValue) {
+                let bestValue = alcoholValue / gasolineValue
+                
+                if bestValue >= 0.7 {
+                    self.resultLabel.text = "Melhor usar Gasolina!"
+                } else {
+                    self.resultLabel.text = "Melhor usar Álcool!"
+                }
+            }
+        }
+    }
+    
+    func validate(alcoholValue: String, gasolineValue: String) -> Bool {
+        var validatedFields = true;
+        
+        if alcoholValue.isEmpty {
+            validatedFields = false;
+        } else if gasolineValue.isEmpty {
+            validatedFields = false;
+        }
+        
+        return validatedFields
+    }
 }
-
